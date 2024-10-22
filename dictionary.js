12ecbd76ba48a2e224c71cae90745a227a844b37
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-10-22 20:01:10 +0800

    Make the second test pass by returning the correct word count

--- dictionary.js
+++ dictionary.js
@@ -1,3 +1,9 @@
-module.exports = function dictionary() {
-  return {}
+module.exports = function dictionary(words) {
+  const result = {}
+
+  if (words) {
+    result[words] = 1
+  }
+
+  return result
 }
