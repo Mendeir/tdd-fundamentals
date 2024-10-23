fa613202f2ebd75d3980eac66acb4c33dd403ae6
Adrianne Magracia <adriannemagracia.git@gmail.com>
2024-10-23 17:49:33 +0800

    Fix spacing from 2 spaces to 4 spaces

--- game-of-life/__tests__/game.spec.js
+++ game-of-life/__tests__/game.spec.js
@@ -2,80 +2,80 @@ require("../game");
 const { isAlive, generate, regenerate, countNeighbours, drawGrid } = window.game;
 
 describe("game of life", () => {
-  describe("isAlive algorithm", () => {
-    test("dead cell with no neighbours", () => {
-      expect(isAlive(0, 0)).toEqual(0);
-    });
-    test("dead cell with 3 neighbours should return 1", () => {
-      expect(isAlive(0, 3)).toEqual(1);
-    });
-    test("live cell with 0 neighbours should return 0", () => {
-      expect(isAlive(1, 0)).toEqual(0);
-    });
-    test("live cell with 2 neighbours should return 1", () => {
-      expect(isAlive(1, 2)).toEqual(1);
-    });
+    describe("isAlive algorithm", () => {
+        test("dead cell with no neighbours", () => {
+            expect(isAlive(0, 0)).toEqual(0);
+        });
+        test("dead cell with 3 neighbours should return 1", () => {
+            expect(isAlive(0, 3)).toEqual(1);
+        });
+        test("live cell with 0 neighbours should return 0", () => {
+            expect(isAlive(1, 0)).toEqual(0);
+        });
+        test("live cell with 2 neighbours should return 1", () => {
+            expect(isAlive(1, 2)).toEqual(1);
+        });
 
-    describe("generate function", () => {
-      test("should create an array of x * x", () => {
-        expect(generate(1)).toEqual([0]);
-        expect(generate(2)).toEqual([0, 0, 0, 0]);
-      });
+        describe("generate function", () => {
+            test("should create an array of x * x", () => {
+                expect(generate(1)).toEqual([0]);
+                expect(generate(2)).toEqual([0, 0, 0, 0]);
+            });
+        });
+
+        describe("countNeighbours", () => {
+            test("should count 1 for array of one", () => {
+                expect(countNeighbours([1])).toEqual(0);
+            });
+            test("should count 2 neighbours", () => {
+                expect(countNeighbours([1, 1, 1, 0], 0)).toEqual(2);
+            })
+            test("should count 2 neighbours", () => {
+                expect(countNeighbours([1, 1, 1, 0], 1)).toEqual(2);
+            })
+            test("should count 2 neighbours", () => {
+                expect(countNeighbours([1, 1, 1, 0], 2)).toEqual(2);
+            })
+            test("should count 3 neighbours", () => {
+                expect(countNeighbours([1, 1, 1, 0], 3)).toEqual(3);
+            })
+            test("should count 3 neighbours", () => {
+                expect(countNeighbours([1, 1, 1, 0, 0, 0, 0, 0, 0], 4)).toEqual(3);
+            })
+        });
+
+        describe("regenerate function", () => {
+            test("should take a generation and update", () => {
+                const cells = generate(1);
+                expect(regenerate(cells)).toEqual(cells);
+            });
+            test("should return all dead cells", () => {
+                const initialCells = generate(2);
+                const cells = generate(2)
+                cells[0] = 1
+                expect(regenerate(cells)).toEqual(initialCells);
+            });
+            test("should return all live cells", () => {
+                const cells = [1, 1, 1, 0]
+                expect(regenerate(cells)).toEqual([1, 1, 1, 1]);
+            })
+        })
+
+        describe("browser grid", () => {
+            test("should display living and dead cells", () => {
+                document.body.innerHTML = '<div id="grid"></div>';
+                drawGrid([0, 0, 1, 1]);
+                expect(document.querySelectorAll(".row").length).toEqual(2);
+                expect(document.querySelectorAll(".cell").length).toEqual(4);
+                expect(document.querySelectorAll(".dead").length).toEqual(2);
+                expect(document.querySelectorAll(".live").length).toEqual(2);
+                drawGrid([1, 1, 0, 0]);
+                expect(document.querySelectorAll(".row").length).toEqual(2);
+                expect(document.querySelectorAll(".cell").length).toEqual(4);
+                expect(document.querySelectorAll(".dead").length).toEqual(2);
+                expect(document.querySelectorAll(".live").length).toEqual(2);
+            });
+        });
+
     });
-
-    describe("countNeighbours", () => {
-      test("should count 1 for array of one", () => {
-        expect(countNeighbours([1])).toEqual(0);
-      });
-      test("should count 2 neighbours", () => {
-        expect(countNeighbours([1, 1, 1, 0], 0)).toEqual(2);
-      })
-      test("should count 2 neighbours", () => {
-        expect(countNeighbours([1, 1, 1, 0], 1)).toEqual(2);
-      })
-      test("should count 2 neighbours", () => {
-        expect(countNeighbours([1, 1, 1, 0], 2)).toEqual(2);
-      })
-      test("should count 3 neighbours", () => {
-        expect(countNeighbours([1, 1, 1, 0], 3)).toEqual(3);
-      })
-      test("should count 3 neighbours", () => {
-        expect(countNeighbours([1, 1, 1, 0, 0, 0, 0, 0, 0], 4)).toEqual(3);
-      })
-    });
-
-    describe("regenerate function", () => {
-      test("should take a generation and update", () => {
-        const cells = generate(1);
-        expect(regenerate(cells)).toEqual(cells);
-      });
-      test("should return all dead cells", () => {
-        const initialCells = generate(2);
-        const cells = generate(2)
-        cells[0] = 1
-        expect(regenerate(cells)).toEqual(initialCells);
-      });
-      test("should return all live cells", () => {
-        const cells = [1, 1, 1, 0]
-        expect(regenerate(cells)).toEqual([1, 1, 1, 1]);
-      })
-    })
-
-    describe("browser grid", () => {
-      test("should display living and dead cells", () => {
-        document.body.innerHTML = '<div id="grid"></div>';
-        drawGrid([0, 0, 1, 1]);
-        expect(document.querySelectorAll(".row").length).toEqual(2);
-        expect(document.querySelectorAll(".cell").length).toEqual(4);
-        expect(document.querySelectorAll(".dead").length).toEqual(2);
-        expect(document.querySelectorAll(".live").length).toEqual(2);
-        drawGrid([1, 1, 0, 0]);
-        expect(document.querySelectorAll(".row").length).toEqual(2);
-        expect(document.querySelectorAll(".cell").length).toEqual(4);
-        expect(document.querySelectorAll(".dead").length).toEqual(2);
-        expect(document.querySelectorAll(".live").length).toEqual(2);
-      });
-    });
-
-  });
 });
--- game-of-life/game.js
+++ game-of-life/game.js
@@ -1,60 +1,60 @@
 const isAlive = (cell, neighbours) =>
-  ((neighbours === 3) || (Boolean(cell) && neighbours === 2)) ? 1 : 0;
+    ((neighbours === 3) || (Boolean(cell) && neighbours === 2)) ? 1 : 0;
 
 const generate = (root) => new Array(root * root).fill(0);
 
 const add = (...args) => args.reduce((accumulator, current) =>
-  accumulator + (current || 0), 0)
+    accumulator + (current || 0), 0)
 
 const leftColumnValues = (index, width, cells) =>
-  index % width
-    ? [cells[index - 1], cells[index - width - 1], cells[index + width - 1]]
-    : [];
+    index % width
+        ? [cells[index - 1], cells[index - width - 1], cells[index + width - 1]]
+        : [];
 
 const rightColumnValues = (index, width, cells) =>
-  (index + 1) % width
-    ? [cells[index + 1], cells[index - width + 1], cells[index + width + 1]]
-    : [];
+    (index + 1) % width
+        ? [cells[index + 1], cells[index - width + 1], cells[index + width + 1]]
+        : [];
 
 const countNeighbours = (cells, index) => {
-  const width = Math.sqrt(cells.length);
-  return add(
-    cells[index - width],
-    cells[index + width],
-    ...leftColumnValues(index, width, cells),
-    ...rightColumnValues(index, width, cells)
-  );
+    const width = Math.sqrt(cells.length);
+    return add(
+        cells[index - width],
+        cells[index + width],
+        ...leftColumnValues(index, width, cells),
+        ...rightColumnValues(index, width, cells)
+    );
 };
 
 const regenerate = cells =>
-  cells.map((cell, index) => isAlive(cell, countNeighbours(cells, index)));
+    cells.map((cell, index) => isAlive(cell, countNeighbours(cells, index)));
 
 const createElement = className => {
-  const element = document.createElement("div");
-  element.className = className;
-  return element;
+    const element = document.createElement("div");
+    element.className = className;
+    return element;
 }
 
 const drawGrid = cells => {
-  const width = Math.sqrt(cells.length); grid = document.getElementById("grid");
-  const container = createElement("container");
-  let row;
-  cells.forEach((cell, index) => {
-    if (index % width === 0) {
-      row = createElement("row");
-      container.appendChild(row);
-    }
-    const cellEl = createElement(`cell ${cell === 0 ? "dead" : "live"}`);
-    row.appendChild(cellEl);
-  });
-  grid.innerHTML = "";
-  grid.appendChild(container);
+    const width = Math.sqrt(cells.length); grid = document.getElementById("grid");
+    const container = createElement("container");
+    let row;
+    cells.forEach((cell, index) => {
+        if (index % width === 0) {
+            row = createElement("row");
+            container.appendChild(row);
+        }
+        const cellEl = createElement(`cell ${cell === 0 ? "dead" : "live"}`);
+        row.appendChild(cellEl);
+    });
+    grid.innerHTML = "";
+    grid.appendChild(container);
 };
 
 window.game = {
-  isAlive,
-  generate,
-  regenerate,
-  countNeighbours,
-  drawGrid
+    isAlive,
+    generate,
+    regenerate,
+    countNeighbours,
+    drawGrid
 };
